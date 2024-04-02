ab986e2aaddd6c00ff35bcddcb612bf05e4b5cbb
Roman Avanesov <rart05@yahoo.com>
2024-04-02 20:31:36 +0300

    1243

--- docx_generation.py
+++ docx_generation.py
@@ -1,13 +1,32 @@
 from docx import Document
-from docx.shared import Inches
+from docx.oxml.ns import qn
+
+from docx.oxml import parse_xml
+
+
+# Пример OMML формулы (здесь должен быть ваш OMML код)
+omml_formula = """
+<m:oMathPara xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math">
+  <m:oMath>
+    <m:sSup>
+      <m:e>
+        <m:r>
+          <m:t>x</m:t>
+        </m:r>
+      </m:e>
+      <m:sup>
+        <m:r>
+          <m:t>2</m:t>
+        </m:r>
+      </m:sup>
+    </m:sSup>
+  </m:oMath>
+</m:oMathPara>
+"""
 
 document = Document()
+p = document.add_paragraph('Вот пример формулы в OMML:')
+omml_el = parse_xml(omml_formula)[0]
+p._p.append(omml_el)
 
-document.add_heading('Вариант 1', 0)
-
-p = document.add_paragraph('ФыафаФыафаыФЫАФ ')
-p.add_run('bold').bold = True
-p.add_run(' and some ')
-p.add_run('italic.').italic = True
-
-document.save('demo.docx')
+document.save('omml_formula.docx')

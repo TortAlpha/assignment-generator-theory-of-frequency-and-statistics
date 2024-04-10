202272738f1c2bc694b34aa6529b6ef8531e6978
Roman Avanesov <rart05@yahoo.com>
2024-04-10 22:45:51 +0300

    interface almost ready

---
+++ MainPanel.py
@@ -0,0 +1,40 @@
+import wx
+import CheckBoxPanel
+
+class MainPanel(wx.Panel):
+    
+    def __init__(self, parent):
+        super(MainPanel, self).__init__(parent)
+        
+        #CheckBoxes
+        self.checkBoxPanel = CheckBoxPanel.CheckBoxPanel(self)
+        
+        self.checkBoxPanel.SetPosition((10, 100))
+        self.checkBoxPanel.SetSize((500, 500))
+        
+            #Setting active all checkboxes
+        for i in range(18):
+            self.checkBoxPanel.checkboxes[i].SetValue(True)
+        
+        #Count of variants
+        self.countVariantsTextBox = wx.TextCtrl(self, pos=(325, 60), size=(30, -1))
+        self.countVariantsTextBox.Bind(wx.EVT_TEXT, self.onTextChange)
+        
+        self.variantsLabel = wx.StaticText(self, label="Введите количество вариантов: ", pos=(120, 62))
+        
+        self.generateButton = wx.Button(self, label="Сгенерировать варианты и ответы", pos=(100,420), size=(300, 25))
+        self.generateButton.Bind(wx.EVT_BUTTON, self.onClickGenerateButton)
+
+        
+    def onTextChange(self, event):
+        content = self.countVariantsTextBox.GetValue()
+        
+        if len(content) > 2:
+            self.countVariantsTextBox.SetValue(content[:2])
+            self.countVariantsTextBox.SetInsertionPointEnd()
+        event.Skip() 
+        
+    def onClickGenerateButton(self, event):
+        pass
+    
+        

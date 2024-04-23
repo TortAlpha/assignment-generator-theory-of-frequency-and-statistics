1a814a5f2c99362fe31a617dc02a27964f342c80
VavakinV <vladvavakin@gmail.com>
2024-04-23 10:32:16 +0300

    Заполнил JSON кроме 15 задачи

--- docx_generation.py
+++ docx_generation.py
@@ -2,7 +2,7 @@ from docx import Document
 import json
 from docx.oxml import parse_xml
 
-def generateDocument(inputFile="taskData.json", taskOutputFile="docx/tasks.docx", answerOutputFile="docx/answers.docx", variants=1,):
+def generateDocument(inputFile="taskData.json", taskOutputFile="docx/tasks.docx", answerOutputFile="docx/answers.docx", variants=1, tasks=range(1,19)):
     with open(inputFile, "r", encoding="utf-8") as file:
         task_data = json.load(file)
 
@@ -17,19 +17,27 @@ def generateDocument(inputFile="taskData.json", taskOutputFile="docx/tasks.docx"
         # Начало варианта
         task_document.add_heading("Вариант "+str(variant+1))
         answer_document.add_heading("Вариант "+str(variant+1))
+        task_number = 1
 
         # Здесь перебор по всем заданиям
-        task_number = 1
-        current_task = task_data[str(task_number)]
-        current_text = current_task['text'].split('/X/')
-        current_data = current_task['data'][variant]
-        current_answer = current_task['answers'][variant]
-        variable_text = str(task_number)+") "
-        for i in range(len(current_data)):
-            variable_text += current_text[i]+current_data[i]
-        variable_text += current_text[-1]
-        task_document.add_paragraph(variable_text)
-        answer_document.add_paragraph(str(task_number)+") "+current_answer)
+        for task in tasks:
+            current_task = task_data[str(task)]
+            # current_text = current_task['text'].split('/X/')
+            # current_data = current_task['data'][variant%variants]
+            current_answer = current_task['answers'][variant%len(current_task['answers'])] if current_task['answers'] is not None else "NONE"
+            # variable_text = str(task_number)+") "
+            # for i in range(len(current_data)):
+            #     variable_text += current_text[i]+current_data[i]
+            # variable_text += current_text[-1]
+
+            # Тест
+            variable_text = current_task['text']
+
+            task_document.add_paragraph(str(task_number)+") "+variable_text)
+            answer_document.add_paragraph(str(task_number)+") "+current_answer)
+
+            
+            task_number += 1
 
         # Здесь конец перебора, добавление новой страницы для следующего варианта
         task_document.add_page_break()
@@ -38,4 +46,4 @@ def generateDocument(inputFile="taskData.json", taskOutputFile="docx/tasks.docx"
     task_document.save(taskOutputFile)
     answer_document.save(answerOutputFile)
 
-generateDocument()
+generateDocument(variants=5, tasks=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18])
